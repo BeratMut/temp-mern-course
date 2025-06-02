274310f7257f1c28064dbd885582cc8a31576f56
Berat Mut <beratmut2@gmail.com>
2025-06-02 18:30:35 +0300

    first commit

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,34 @@
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+  BadRequestError,
+} from "../errors/customError.js";
+import { verifyJWT } from "../utils/tokenUtils.js";
+
+export const authenticateUser = (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError("authentication invalid");
+
+  try {
+    const { userId, role } = verifyJWT(token);
+    const testUser = userId === "6835c3f1affebb7d790d78cf";
+    req.user = { userId, role, testUser };
+    next();
+  } catch (error) {}
+};
+
+export const authorizePermissions = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      throw new UnauthorizedError("Unauthorized to access this route");
+    }
+    next();
+  };
+};
+
+export const checkForTestUser = (req, res, next) => {
+  if (req.user.testUser) {
+    throw new BadRequestError("Demo User. Read Only'");
+  }
+  next();
+};
---
+++ server.js
@@ -0,0 +1,76 @@
+import "express-async-errors";
+import express from "express";
+import morgan from "morgan";
+import * as dotenv from "dotenv";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+//routers
+import jobRouter from "./routes/jobRouter.js";
+import authRouter from "./routes/authRouter.js";
+import userRouter from "./routes/userRouter.js";
+//public
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import path from "path";
+//middleware
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import { authenticateUser } from "./middleware/authMiddleware.js";
+import cloudinary from "cloudinary";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+
+dotenv.config();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/api/v1/test", (req, res) => {
+  res.json({ msg: "test route" });
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello Word");
+});
+
+app.post("/", (req, res) => {
+  console.log(req);
+  res.json({ message: "data received", data: req.body });
+});
+
+app.use("/api/v1/jobs", authenticateUser, jobRouter);
+app.use("/api/v1/users", authenticateUser, userRouter);
+app.use("/api/v1/auth", authRouter);
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5100;
+
+try {
+  await mongoose.connect(process.env.MONGO_URL);
+  app.listen(port, () => {
+    console.log("Server running on " + port);
+  });
+} catch (error) {
+  console.log(error);
+  process.exit(1);
+}
